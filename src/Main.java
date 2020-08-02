56dc8ede9ec424405538c3d5881563ae6a4172e2
sushi5487 <vanquishdestroyconquer@gmail.com>
2020-08-02 12:13:02 -0400

    My first push

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args){
+        String miNombre;
+        int miEdad;
+        miNombre = "AC";
+        System.out.println("Print This");
+    }
+}
+
+
